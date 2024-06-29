74f2e431d89f10c5bae07234b179035b14f92c5e
Kanailal Manna <kanailalmanna2003@gmail.com>
2024-06-29 07:35:52 +0530

    cors fix

--- server/src/app.js
+++ server/src/app.js
@@ -4,17 +4,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    method: "*",
-  }),
-);
+// Configure CORS to allow all origins
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 import authRouter from "./routes/auth.router.js";
 
